37541ba460cf8778b48bf15bbe54d5a246b9f144
chiragpattanaik <chiragrocks02@gmail.com>
2024-02-23 21:43:47 +0530

    added pywin32==306; platform_system == "Windows"

--- templates/app.py
+++ templates/app.py
@@ -5,7 +5,7 @@ import pymongo
 app = Flask(__name__)
 
 
-def sql_table_queries(op, name, values):
+def sql_table_queries(op, name, values,  col1=None, col2=None, val1=None, val2=None):
     if op.lower() == "tablecreation":
         query = f'CREATE TABLE {name} (ID INT(10), Name VARCHAR(20), GENDER VARCHAR(10), AGE INT(10))'
         return query, values
@@ -19,13 +19,18 @@ def sql_table_queries(op, name, values):
         query = f'DROP TABLE {name}'
         return query, values
 
+    elif op.lower() == "update":
+        query = f'UPDATE {name} SET {col1} = %s WHERE {col2} = %s'
+        values = (val1, val2)
+        return query, values
 
-def perform_sqldb_operation(db_operation, op, table_name, values=None):
+
+def perform_sqldb_operation(db_operation, op, table_name, values=None, col1=None, col2=None, val1=None, val2=None):
     try:
         if db_operation.lower() == "sql":
             conn = connection.connect(host="localhost", user="root", passwd="mysql123", database='razer', use_pure=True)
             cursor = conn.cursor()
-            query, values = sql_table_queries(op, table_name, values)
+            query, values = sql_table_queries(op, table_name, values, col1, col2, val1, val)
             if op.lower() == 'insertion':
                 if len(values) > 1:
                     cursor.executemany(query, values)
@@ -71,12 +76,22 @@ def choose_db():
             data = request.get_json()
             operation = request.json['operation']
             op_perform = request.json['op']
-            tablename = request.json['tablename']
+            column1 = request.json['col1']
+            column2 = request.json['col2']
+            value1 = request.json['val1']
+            value2 = request.json['val2']
             values = data.get('values')
-            result = perform_sqldb_operation(operation, op_perform, tablename, values)
+            Tablename = request.json['tablename']
+
+            if op_perform.lower() == 'update':
+                result = perform_sqldb_operation(operation, op_perform, Tablename, values, column1, column2, value1, value2)
+            else:
+                result = perform_sqldb_operation(operation, op_perform, Tablename, values)
+
             return jsonify(result=result)
     except Exception as e:
         print(str(e))
+        return jsonify(error=str(e)), 500
 
 
 @app.route('/MongoDB', methods=['POST'])
